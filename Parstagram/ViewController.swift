f644b5776f4f32ee5a21ec2090ef36095247570d
Ziyi Ye <ziy116@ucsd.edu>
2019-04-04 20:22:09 -0700

    Initial Commit

---
+++ Parstagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Parstagram
+//
+//  Created by Ziyi Ye on 4/4/19.
+//  Copyright © 2019 Frank Ye. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
