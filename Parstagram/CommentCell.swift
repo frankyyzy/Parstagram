59f6035ab52394ed773c8fb9549aeef57539067e
Ziyi Ye <ziy116@ucsd.edu>
2019-04-09 21:33:59 -0700

    halfway

---
+++ Parstagram/CommentCell.swift
@@ -0,0 +1,27 @@
+//
+//  CommentCell.swift
+//  Parstagram
+//
+//  Created by Ziyi Ye on 4/9/19.
+//  Copyright © 2019 Frank Ye. All rights reserved.
+//
+
+import UIKit
+
+class CommentCell: UITableViewCell {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var commentLabel: UILabel!
+     
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
